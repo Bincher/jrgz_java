4d872f71dcd9ce8420f549ac17e3b97388596743
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-01 20:08:25 +0900

    01.기본알고리즘 / 연습 Q7. 1+2+3+4+5+6+7=28

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,5 +1,5 @@
 //01.기본알고리즘
-//연습 Q6. WHILE문과 변수 I값 확인
+//연습 Q7. 1+2+3+4+5+6+7=28
 
 package java_jrgz;
 import java.util.Scanner;
@@ -8,16 +8,17 @@ public class java_jrgz {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
-		int i = 1;
 		int sum = 0;
+		int i;
 		
-		while (i <= a)
+		for (i = 1; i <= a; i++)
 		{
 			sum += i;
-			i++;
+			System.out.print(i);
+			if (i != a)
+				System.out.print("+");
 		}
-		
-		System.out.print("sum = " + sum + " / i = " + i);
+		System.out.print("=" + sum);
 	}
 
 }
