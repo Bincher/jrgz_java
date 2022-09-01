1345129fc8f4609c370f6bb4f4521470074b2ecb
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-01 19:08:41 +0900

    01.기본알고리즘 / 연습 Q1. 4개의 수 최댓값

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,23 +1,25 @@
 //01.기본알고리즘
-//실습 1-2 최댓값리턴클래스
+//연습 Q1. 4개의 수 최댓값
 
 package java_jrgz;
 //max3라는 내부클래스 생성
 public class java_jrgz {
-	static int max3(int a, int b, int c) {
+	static int max4(int a, int b, int c, int d) {
 		//max에 a삽입
 		int max = a;
-		//b or c 가 max보다 클시 if문을 이용해 삽입
+		//b or c 가 max보다 클시 if문을 이용해 삽입(elseif를 왜쓰나)
 		if (b>max)
 			max = b;
-		else if (c>max)
+		if (c>max)
 			max = c;
+		if (d>max)
+			max = d;
 		//max 반환
 		return max;
 	}
 	public static void main(String[] args) {
 		//예시로 1,3,2
-		int num = max3(1,3,2);
+		int num = max4(1,2,3,2);
 		System.out.print(num);
 		//테스트
 	}
