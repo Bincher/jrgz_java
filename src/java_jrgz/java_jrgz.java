4e9c488eda97c48f212d6313ba51ad4e201a4ad8
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-01 19:11:25 +0900

    01.기본알고리즘 / 연습 Q2. 3개의 수 최솟값

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,27 +1,24 @@
 //01.기본알고리즘
-//연습 Q1. 4개의 수 최댓값
+//연습 Q2. 3개의 수 최솟값
 
 package java_jrgz;
-//max3라는 내부클래스 생성
+
 public class java_jrgz {
-	static int max4(int a, int b, int c, int d) {
-		//max에 a삽입
-		int max = a;
-		//b or c 가 max보다 클시 if문을 이용해 삽입(elseif를 왜쓰나)
-		if (b>max)
-			max = b;
-		if (c>max)
-			max = c;
-		if (d>max)
-			max = d;
-		//max 반환
-		return max;
+	static int min3(int a, int b, int c) {
+		
+		int min = a;
+		
+		if (b<min)
+			min = b;
+		if (c<min)
+			min = c;
+		
+		return min;
+		
 	}
 	public static void main(String[] args) {
-		//예시로 1,3,2
-		int num = max4(1,2,3,2);
+		int num = min3(3,1,2);
 		System.out.print(num);
-		//테스트
 	}
 
 }
