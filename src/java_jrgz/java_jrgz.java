8468ef1459b1f628834596d24d1033db9576463d
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-01 20:47:47 +0900

    01.기본알고리즘 / 연습 Q10. a와 b 입력하고 b-a 구하기

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,33 +1,24 @@
 //01.기본알고리즘
-//연습 Q9. a와 b 사이의 총합 
+//연습 Q10. a와 b 입력하고 b-a 구하기 
 
 package java_jrgz;
 import java.util.Scanner;
 
 public class java_jrgz {
-	static int sumof(int a, int b)
-	{
-		int i = 0;
-		int sum = 0;
-		if (a >= b)
-		{
-			i = a;
-			a = b;
-			b = i;
-		}
-		for (i = a; i <= b; i++)
-		{
-			sum += i;
-		}
-		return sum;
-	}
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.print("a 입력 : ");
-		int a = s.nextInt();
-		System.out.print("b 입력 : ");
-		int b = s.nextInt();
-		System.out.print(sumof(a,b));
+		int a,b;
+		while(true)
+		{
+			System.out.print("a 입력 : ");
+			a = s.nextInt();
+			System.out.print("b 입력 : ");
+			b = s.nextInt();
+			if (a>=b)
+				System.out.println("a보다 큰 값을 입력하세요!");
+			else
+				break;
+		}
+		System.out.print(b-a);
 	}
-
 }
