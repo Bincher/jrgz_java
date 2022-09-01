aa045ca662f0908097eb16dfc27ffd6a94d6194a
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-01 19:58:21 +0900

    01.기본알고리즘 / 연습 Q6. WHILE문과 변수 I값 확인

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,36 +1,23 @@
 //01.기본알고리즘
-//실습 1C-1. 중앙값구하기
+//연습 Q6. WHILE문과 변수 I값 확인
 
 package java_jrgz;
 import java.util.Scanner;
 
 public class java_jrgz {
-	static int med3(int a, int b, int c) {
-		if (a>=b)
-		{
-			if (b>=c)
-				return b;
-			else if (a>=c)
-				return c;
-			else
-				return a;
-		}
-		else if (a>c)
-			return a;
-		else if (b>c)
-			return c;
-		else
-			return b;
-	}
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.print("a : ");
 		int a = s.nextInt();
-		System.out.print("b : ");
-		int b = s.nextInt();
-		System.out.print("c : ");
-		int c = s.nextInt();
-		System.out.print("중앙값 : " + med3(a,b,c));
+		int i = 1;
+		int sum = 0;
+		
+		while (i <= a)
+		{
+			sum += i;
+			i++;
+		}
+		
+		System.out.print("sum = " + sum + " / i = " + i);
 	}
 
 }
