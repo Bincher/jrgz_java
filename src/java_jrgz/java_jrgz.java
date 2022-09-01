04dc74859c8ba4e3e619c16c82785cb2ff5a4952
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-01 20:16:43 +0900

    01.기본알고리즘 / 연습 Q8. 가우스덧셈

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,5 +1,5 @@
 //01.기본알고리즘
-//연습 Q7. 1+2+3+4+5+6+7=28
+//연습 Q8. 가우스덧셈
 
 package java_jrgz;
 import java.util.Scanner;
@@ -7,18 +7,16 @@ import java.util.Scanner;
 public class java_jrgz {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int a = s.nextInt();
+		int n = s.nextInt();
 		int sum = 0;
-		int i;
-		
-		for (i = 1; i <= a; i++)
+		if (n % 2 == 0)
 		{
-			sum += i;
-			System.out.print(i);
-			if (i != a)
-				System.out.print("+");
+			sum = (1 + n) * (n / 2);
 		}
-		System.out.print("=" + sum);
+		else
+			sum = (n) * (n / 2 + 1);
+		
+		System.out.print(sum);
 	}
 
 }
