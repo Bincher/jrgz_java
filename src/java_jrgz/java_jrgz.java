219db26f009f0f5123daa2660d2e85c404f93bcb
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-02 14:15:11 +0900

    02.기본 자료구조 / 연습 Q3. 배열의 합

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,25 +1,17 @@
 //02.기본 자료구조
-//연습 Q2. 배열 요소 역순과 과정 설명
+//연습 Q3. 배열의 합
 
 package java_jrgz;
 import java.util.*;
 
 public class java_jrgz {
-	static void swap(int [] arr, int size){
-		int temp = 0;
-		for(int i = 0; i < (size/2); i++)
+	static int sumOf(int [] arr){
+		int result = 0;
+		for (int i = 0; i < arr.length; i++)
 		{
-			System.out.print("arr[" + i + "]" + "과(와) arr[" + (size-i-1) + "]를 교환합니다.\n");
-			temp = arr[size-i-1];
-			arr[size-i-1] = arr[i];
-			arr[i] = temp;
-			for (int j = 0; j < arr.length; j++)
-			{
-				System.out.print(arr[j] + " ");
-			}
-			System.out.println();
+			result += arr[i];
 		}
-		
+		return result;
 	}
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -33,6 +25,7 @@ public class java_jrgz {
 			System.out.print((i+1) + "번째 요소 입력 : ");
 			arr[i] = s.nextInt();
 		}
-		swap(arr, size);
+		
+		System.out.print("합 : " + sumOf(arr));
 	}
 }
