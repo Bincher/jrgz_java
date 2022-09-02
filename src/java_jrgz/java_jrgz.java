98e39e9628ea64bfe9defc491cace896f2799e1b
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-02 14:24:16 +0900

    02.기본 자료구조 / 연습 Q4. copy

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,31 +1,40 @@
 //02.기본 자료구조
-//연습 Q3. 배열의 합
+//연습 Q4. copy
 
 package java_jrgz;
 import java.util.*;
 
 public class java_jrgz {
-	static int sumOf(int [] arr){
-		int result = 0;
-		for (int i = 0; i < arr.length; i++)
+	static void copy(int [] arr1, int [] arr2){
+		for (int i = 0; i < arr1.length; i++)
 		{
-			result += arr[i];
+			arr2[i] = arr1[i];
 		}
-		return result;
 	}
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.print("배열의 크기 지정 : ");
 		int size = s.nextInt();
 		
-		int [] arr = new int[size];
+		int [] arr1 = new int[size];
+		int [] arr2 = new int[size];
 		
-		for (int i = 0; i < arr.length; i++)
+		for (int i = 0; i < arr1.length; i++)
 		{
 			System.out.print((i+1) + "번째 요소 입력 : ");
-			arr[i] = s.nextInt();
+			arr1[i] = s.nextInt();
 		}
 		
-		System.out.print("합 : " + sumOf(arr));
+		copy(arr1, arr2);
+		
+		for (int i = 0; i < arr1.length; i++)
+		{
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < arr2.length; i++)
+		{
+			System.out.print(arr2[i] + " ");
+		}
 	}
 }
