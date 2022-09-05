53a70b46209cc461cee4bb8f24d5eb2a69984b83
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-05 21:16:12 +0900

    02.기본 자료구조 / 실습 2-14. 신체검사

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,24 +1,73 @@
 //02.기본 자료구조
-//실습 2C-1. for-each문
+//실습 2-14. 신체검사
 
 package java_jrgz;
 import java.util.*;
 
 public class java_jrgz {
-	public static void main(String[] args) {
-		double[] a = {1.0, 2.0, 3.0, 4.0, 5.0};
+	
+	static final int VMAX = 21;
+	
+	static class PhyscDate{
+		String name;
+		int height;
+		double vision;
 		
-		for (int i = 0; i < a.length; i++)
+		PhyscDate(String name, int height, double vision)
 		{
-			System.out.println("a[" + i + "] = " + a[i]);
+			this.name = name;
+			this.height = height;
+			this.vision = vision;
 		}
+	}
+	
+	static double aveHeight(PhyscDate[] dat) {
 		double sum = 0;
 		
-		for(double i : a)
+		for (int i = 0; i < dat.length; i++)
 		{
-			sum += i;
+			sum += dat[i].height;
 		}
 		
-		System.out.println("배열의 합은 " + sum);
+		return sum/dat.length;
+	}
+	
+	static void distVision(PhyscDate[] dat, int dist[]) {
+		int i = 0;
+		
+		dist[i] = 0;
+		for (i = 0; i < dat.length; i++)
+		{
+			if (dat[i].vision >= 0.0 && dat[i].vision <= VMAX/10.0)
+				dist[(int)(dat[i].vision * 10)]++;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		PhyscDate[] x = {
+				new PhyscDate("박현규", 162, 0.3),
+				new PhyscDate("함진아", 173, 0.7),
+				new PhyscDate("최윤미", 175, 2.0),
+				new PhyscDate("홍연의", 171, 1.5),
+				new PhyscDate("이수진", 168, 0.4),
+				new PhyscDate("김영준", 174, 1.2),
+				new PhyscDate("박용규", 169, 0.8),
+		};
+		int[] vdist = new int[VMAX];
+		
+		System.out.print("<신체검사 리스트>\n이름       키 시력\n----------------\n");
+		for (int i = 0; i < x.length; i++)
+		{
+			System.out.printf("%-8s%3d%5.1f\n", x[i].name, x[i].height, x[i].vision);
+		}
+		
+		distVision(x, vdist);
+		
+		System.out.println("\n시력분포");
+		for (int i = 0; i < VMAX; i++)
+		{
+			System.out.printf("%3.1f~ :2%d명\n", i / 10.0, vdist[i]);
+		}
 	}
 }
