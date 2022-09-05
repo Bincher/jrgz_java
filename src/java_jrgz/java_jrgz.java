b093a0b0a6aea44b415d0fc7044fbb2cc9dca633
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-05 19:37:20 +0900

    02.기본 자료구조 / 실습 2-13. 윤년포함한 날짜계산

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,41 +1,42 @@
 //02.기본 자료구조
-//실습 2-11. 소수찾기(제곱근 사용) 
+//실습 2-13. 윤년포함한 날짜계산 
 
 package java_jrgz;
 import java.util.*;
 
 public class java_jrgz {
-	public static void main(String[] args) {
-		int counter = 0;
-		int ptr = 0;
-		int[] prime = new int[500];
+	static int [][] mdays = {
+			{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+			{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	};
+	static int isLeap(int year) {
+		return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
+	}
+	static int dayOfYear(int y, int m, int d) {
+		int days = d;
 		
-		prime[ptr++] = 2;
-		prime[ptr++] = 3;
-		
-		for(int n = 5; n <= 1000; n++)
+		for (int i = 1; i < m; i++)
 		{
-			boolean flag = false;
-			for (int i = 0; prime[i] * prime[i] <= n; i++)
-			{
-				counter += 2;
-				if (n % prime[i] == 0)
-				{
-					flag = true;
-					break;
-				}
-			}
-			if (!flag)
-			{
-				prime[ptr++] = n;
-				counter++;
-			}
-			
-			for (int i = 0; i < ptr; i++)
-			{
-				System.out.println(prime[i]);
-			}
-			System.out.println("곱셈과 나눗셈을 수행한 횟수 : "+ counter);
+			days += mdays[isLeap(y)][i - 1];
 		}
+		return days;
+	}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int retry;
+		
+		do {
+			System.out.print("년 : ");
+			int year = s.nextInt();
+			System.out.print("월 : ");
+			int month = s.nextInt();
+			System.out.print("일 : ");
+			int day = s.nextInt();
+			
+			System.out.printf("그 해 %d일째입니다.\n", dayOfYear(year,month,day));
+			
+			System.out.print("다시 하시겠습니까?(1=yes, 0=no) : ");
+			retry = s.nextInt();
+		}while(retry == 1);
 	}
 }
