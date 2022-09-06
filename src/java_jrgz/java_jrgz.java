80008f798cb5cc04b93fe0ae9014da3feebab97c
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-06 15:55:49 +0900

    03.검색 / 실습 3-3. 선형 검색(보초법)

--- src/java_jrgz/java_jrgz.java
+++ src/java_jrgz/java_jrgz.java
@@ -1,77 +1,40 @@
-//02.기본 자료구조
-//연습 Q11. 서기 년월일과 날짜반환 그리고 클래스
+//03.검색
+//실습 3-3. 선형 검색(보초법)
 
 package java_jrgz;
 import java.util.*;
 
 public class java_jrgz {
 	
-	
-	static class YMD{
+	static int swqSearchSen(int[] a, int n, int key){
 		
-		int [][] dYear = {
-				{31,29,31,30,31,30,31,31,30,31,30,31},
-				{31,28,31,30,31,30,31,31,30,31,30,31},
-		};
-		
-		int getYear(int y) {
-			if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
-				return 0;
-			else
-				return 1;
+		int i = 0; 
+		while(true) {
+			if (a[i] == key)
+				break;
+			i++;
 		}
+		return (i == n) ? -1 : i;
 		
-		int y;
-		int m;
-		int d;
-		
-		public YMD(int y, int m, int d){
-			this.y = y;
-			this.m = m;
-			this.d = d;
-		}
-		
-		public YMD before(int n) {
-			if (n < 0)
-			      return after(-n);
-			YMD temp = new YMD(y, m, d);
-		    temp.d -= n;
-		    while (temp.d < 1) 
-		    {
-		      temp.d += dYear[getYear(temp.y)][temp.m -1];
-		      if (--temp.m == 0) 
-		      {
-		        temp.y--;
-		        temp.m = 12;
-		      }
-		    }
-		    return temp;
-		}
-		
-		public YMD after(int n) {
-			if (n < 0)
-			      return before(-n);
-			
-			YMD temp = new YMD(y, m, d);
-		    temp.d += n;
-		    while (temp.d > dYear[getYear(y)][temp.m - 1]) 
-		    {
-		      temp.d -= dYear[getYear(y)][temp.m-1];
-		      if (++temp.m == 13) 
-		      {
-		        temp.y++;
-		        temp.m = 1;
-		      }
-		    }
-		    return temp;
-		}
 	}
+	
 	public static void main(String[] args) {
-		YMD a = new YMD(2022, 06, 16);
-		a = a.after(31);
-		System.out.print(a.y + "년 " + a.m + "월 " + a.d + "일\n");
-		a = a.before(31);
-		System.out.print(a.y + "년 " + a.m + "월 " + a.d + "일\n");
+		Scanner s = new Scanner(System.in);
 		
+		System.out.print("요소수 : ");
+		int num = s.nextInt();
+		int [] arr = new int[num + 1];
+		
+		for (int i = 0; i < num; i++) {
+			System.out.print("arr[" + i + "] : ");
+			arr[i] = s.nextInt();
+		}
+		
+		System.out.print("검색할 값 : ");
+		arr[num] = s.nextInt();
+		
+		int idx = swqSearchSen(arr, num, arr[num]);
+		
+		System.out.print(arr[num] + "은(는) x[" + idx + "]에 있습니다.");
 	}
 }
